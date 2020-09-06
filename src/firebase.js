a2b100d7889fc0a86097a006a3fb88bd6a46c37c
kevin <kevinrangels>
2020-09-06 19:20:44 -0430

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCnOBYCi46-i47Q26EYj9utKTHn_MrCpfQ",
+    authDomain: "clone-257a1.firebaseapp.com",
+    databaseURL: "https://clone-257a1.firebaseio.com",
+    projectId: "clone-257a1",
+    storageBucket: "clone-257a1.appspot.com",
+    messagingSenderId: "660978607122",
+    appId: "1:660978607122:web:e8ddf8f59fcc43f94de4cb"
+};
